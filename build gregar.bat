f0b106ef0f1bd18591ad73fad70c592d5021bd68
ssbmars <46971717+ssbmars@users.noreply.github.com>
2024-07-13 00:08:38 -0700

    update patch names

--- build gregar.bat
+++ build gregar.bat
@@ -1,5 +1,5 @@
 @echo off
 armips Taisen_GXX.asm -sym Taisen_Gregar.sym
 textpet run-script exe6gregar.tps
-flips -c -b "Gregar\exe6gregar.gba" "idealEXE6 Gregar.gba" "patch/idealEXE6 Gregar.bps"
+flips -c -b "Gregar\exe6gregar.gba" "idealEXE6 Gregar.gba" "patch/BR5J_00.bps"
 timeout 3
