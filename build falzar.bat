fe6e6a959ac46be3cfc9bdb79babbc6bf88ea290
ssbmars <46971717+ssbmars@users.noreply.github.com>
2024-07-13 00:11:42 -0700

    update patch names

--- build falzar.bat
+++ build falzar.bat
@@ -1,5 +1,5 @@
 @echo off
 armips Taisen_RXX.asm -sym Taisen_Falzar.sym
 textpet run-script exe6falzar.tps
-flips -c -b "Falzar\exe6falzar.gba" "idealEXE6 Falzar.gba" "patch/idealEXE6 Falzar.bps"
+flips -c -b "Falzar\exe6falzar.gba" "idealEXE6 Falzar.gba" "patch/BR6J_00.bps"
 timeout 3
